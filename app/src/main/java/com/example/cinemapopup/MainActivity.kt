94d53cd70bac3acd7b7f2e767a75af268ac1b9b9
l0rddarkside <viictor.alvess12@gmail.com>
2019-04-17 22:12:21 -0300

    Funcionamento do ReciclerView na MainActivity

--- app/src/main/java/com/example/cinemapopup/MainActivity.kt
+++ app/src/main/java/com/example/cinemapopup/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.cinemapopup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val minhaLista = movies_list
+        val adapter = MoviesAdapter()
+        minhaLista.adapter = adapter
+        minhaLista.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
     }
 }
